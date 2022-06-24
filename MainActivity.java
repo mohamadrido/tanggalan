f0e7c0484978b5e23c35a25cbe05ac5967866219
mohamadrido <54422386+mohamadrido@users.noreply.github.com>
2022-06-24 10:46:58 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+package com.mohamadrido.tanggalan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
